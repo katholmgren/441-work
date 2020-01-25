d6df1514f054501923056b14f64ffa6a82cb45ab
Katherine <holmgrenkatherine@gmail.com>
2020-01-25 16:57:40 -0700

    HW-2

---
+++ HW-2/README.md
@@ -0,0 +1,9 @@
+Katherine Holmgren
+
+January 25, 2020
+
+# HW-2 Response
+
+[HW-2 Live Link](https://katholmgren.github.io/441-work/HW-2)
+
+In this week's homework cycle, we learned about the JavaScript Console. We had to make a webpage that displayed our three favorite websites, games, and artists in the console. This was review for me. On the other hand, I didn't realize that other can people can see what is written in the console. I had no difficulties during this assignment. I will most likely review the last few weeks from MART 341 this weekend.
---
+++ HW-2/script.js
@@ -0,0 +1,17 @@
+// Three Favorite Websites
+console.log("Three Favorite Webistes");
+console.log("https://www.metmuseum.org/");
+console.log("https://librivox.org/");
+console.log("https://www.pinterest.com/");
+
+// Three Favorite Games
+console.log("Three Favorite Games");
+console.log("Super Smash Brothers");
+console.log("HerInteractive Nancy Drew");
+console.log("Minecraft");
+
+// Three Favorite Artists
+console.log("Three Favorite Artists");
+console.log("Chris Riddell");
+console.log("Joshua Barkman (False Knees)");
+console.log("Maggie Cole");
