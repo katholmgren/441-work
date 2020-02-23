7012cb14e803768de8bf6fadcd26bf646137ef11
Katherine <holmgrenkatherine@gmail.com>
2020-02-22 18:14:34 -0700

    Stored more info in JSON

--- HW-6/main.js
+++ HW-6/main.js
@@ -1,42 +1,28 @@
-// create an array of image names that correspond to the image tags
+
 var imageNames = ["image1", "image2", "image3", "image4", "image5", "image6","image7", "image8", "image9", "image10", "image11", "image12"];
 var blankImagePath = "imgs/blank-filler.jpg"; // create a variable with the blank image name
-
 var firstNumber = -1;
 var secondNumber = -1;
-var misses = 0; // Starting miss count
-var player = {"firstname":"", "lastname":""}; // JSON name declaration
-var score = {"scoreTotal":""}; // JSON score declaration
-var actualImages = new Array(); // create a empty array for the actual images
+var misses = 0;
+var player = {"firstname":"", "lastname":"", "age":""}; // JSON name declaration
+var actualImages = new Array(); // create an empty array for the actual images
 
-
-function printBlanks()
-{
+function printBlanks(){
    // call our random image creation function
     createRandomImageArray();
     // create a for loop
-    for(var i = 0; i < imageNames.length; i++)
-    {
+    for(var i = 0; i < imageNames.length; i++){
     // iterate through the image tag ids and sets the source
         document.getElementById(imageNames[i]).src= blankImagePath;
     }
 }
 
-function createRandomImageArray()
-{
-    // create an array of actual images
+function createRandomImageArray(){
     var actualImagePath = ["imgs/flauna-edit.jpg", "imgs/hedge-edit.jpg", "imgs/mushroom-edit.jpg", "imgs/october-edit.jpg", "imgs/system-edit.jpg", "imgs/vangogh-edit.jpg"];
-    // create another array to make sure the images only get added twice
-    var count = [0,0,0,0,0,0];
-    // create a while statement to check to see if our actual image array is full
-    while(actualImages.length < 12)
-    {
-        // get a random number between 0 and the number total number of images that we can choose from
+    var count = [0,0,0,0,0,0]; // make sure the images only get added twice
+    while(actualImages.length < 12){ // check to see if our actual image array is full
         var randomNumber = Math.floor(Math.random() * actualImagePath.length)
-          // create an if statement that says if the total number added is less than 6, then
-        // add the image to the actual image array
-        if(count[randomNumber] < 2)
-        {
+        if(count[randomNumber] < 2){
             actualImages.push(actualImagePath[randomNumber]);
             // then add one to the array that makes sure only two images can be added
             count[randomNumber] = count[randomNumber] + 1;
@@ -44,41 +30,27 @@ function createRandomImageArray()
     }
 }
 
-function flipImage(number)
-{
-
-    // make the second image appear
-    if(firstNumber >= 0)
-    {
+function flipImage(number){
+    if(firstNumber >= 0){ // make the second image appear
         secondNumber = number;
         document.getElementById(imageNames[number]).src = actualImages[secondNumber];
-
     }
-    else if(firstNumber < 0) // make the first image appear
-    {
+    else if(firstNumber < 0){ // make the first image appear
         firstNumber = number;
         document.getElementById(imageNames[firstNumber]).src= actualImages[firstNumber];
-
     }
-
-    // check to see if the images do not match
-    if(actualImages[secondNumber] != actualImages[firstNumber] && firstNumber >= 0 && secondNumber >= 0)
-    {
+    // check to see if the images DO NOT match
+    if(actualImages[secondNumber] != actualImages[firstNumber] && firstNumber >= 0 && secondNumber >= 0){
         setTimeout(imagesDisappear, 1000); // calls a method after 1 second
-
     }
-    // check to see if the images do match
-    else if(actualImages[secondNumber] == actualImages[firstNumber] && firstNumber >= 0 && secondNumber >= 0)
-    {
+    // check to see if the images DO match
+    else if(actualImages[secondNumber] == actualImages[firstNumber] && firstNumber >= 0 && secondNumber >= 0){
         firstNumber = -1;
         secondNumber = -1;
     }
-
 }
 
-function imagesDisappear()
-{
-
+function imagesDisappear(){
     console.log(firstNumber);
     document.getElementById(imageNames[firstNumber]).src = blankImagePath;
     document.getElementById(imageNames[secondNumber]).src = blankImagePath;
@@ -88,48 +60,26 @@ function imagesDisappear()
     if (firstNumber === -1){
     misses += 1;
     myMiss.innerHTML = misses;
-
-  }
+    }
 }
 
-
-
-
-// add to the JSON from the textboxes
-function addToPlayer()
-{
+function addToPlayer(){ // add to the JSON from the textboxes
     var firstName = document.getElementById("txtFirstName").value;
+    var lastName = document.getElementById("txtLastName").value;
+    var age = document.getElementById("txtAge").value;
+
     //console.log(firstName);
     player.firstname = firstName;
+    player.lastname = lastName;
+    player.age = age;
     localStorage.setItem("playerInfo", JSON.stringify(player));
     window.location = "game.html";
 }
 
-// get the information out of JSON
-function playerInfo()
-{
+function playerInfo(){ // get the information out of JSON
     var playerInformation = localStorage.getItem("playerInfo");
     player = JSON.parse(playerInformation);
     console.log(player.firstname);
-}
-
-
-
-
-// add to the JSON from the score
-function addToScore()
-{
-    var score = document.getElementById("score").value;
-    //console.log(score);
-    score.scoreTotal = score;
-    localStorage.setItem("scoreTotal", JSON.stringify(score));
-    window.location = "outro.html";
-}
-
-// get the information out of JSON
-function scoreInfo()
-{
-    var scoreInformation = localStorage.getItem("scoreInfo");
-    player = JSON.parse(scoreInformation);
-    console.log(score.scoreTotal);
+    console.log(player.lastname);
+    console.log(player.age);
 }
