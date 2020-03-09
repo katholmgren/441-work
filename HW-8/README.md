784dc8aa11bdcd650201eab93f398be12790e46e
Katherine <holmgrenkatherine@gmail.com>
2020-03-09 16:12:04 -0600

    Finished comments and README

---
+++ HW-8/README.md
@@ -0,0 +1,26 @@
+Katherine Holmgren
+
+March 9, 2020
+
+# HW-8 Response
+
+[HW-8 Live Link](https://katholmgren.github.io/441-work/HW-8)
+
+We used JQuery in this week's homework for the first time. I liked using JQuery because it made coding in JavaScript much simpler. I was able to make something complex without as much time or logic. Since we had to have elements fade in and out in this assignment, I decided to use clouds because they naturally fade in and out in the real world.
+
+### Process
+
+1. Set up files
+2. Downloaded images
+3. Put temporary borders on clouds in CSS for easy identification
+4. Placed clouds in the correct starting places
+5. Moved clouds around in JQuery
+6. Added ending text
+7. Added beginning text
+8. Added sun
+
+### Difficulties
+
+I didn't have too many difficulties in this assignment. The most trouble I had was figuring out the right way to hide and show elements. I'm also not very confident in using JQuery with JavaScript and object-oriented programming. That's why I focused on only using JQuery this week.
+
+![HW-8 Screenshot](./imgs/HW-8-screenshot.png)
--- HW-8/main.js
+++ HW-8/main.js
@@ -1,46 +1,51 @@
+// Katherine Holmgren
+// HW-8 Main JS
+
+
 $(function(){
 
 });
 
 $(document).ready(function(){
+    // Originally hidden elements:
+    $("#cloud-3").hide();
+    $("#sun").hide();
 
-  $("#cloud-3").hide();
-  $("#sun").hide();
-    // RED Dark Right Hand Corner
+    // Beginning Text
+    $("#clickAround").hover(function(){
+      $("#clickAround").fadeOut(1500);
+    });
+
+    // CLOUD ONE (Dark Right Hand Corner)(Red Border)
     $("#cloud-1").click(function(){
         $("#cloud-1").animate({left: '250px'}, 1000);
         $("#cloud-1").fadeOut(1000);
     });
 
-    // ORANGE Dark Bottom
+    // CLOUD TWO (Dark Bottom)(Orange Border)
     $("#cloud-2").click(function(){
         $("#cloud-2").animate({right: '250px'}).fadeOut(1000, function(){
-          $("#cloud-3").fadeIn("slow");
+          $("#cloud-3").fadeIn("slow"); // Display Cloud 3
         });
     });
 
+    // CLOUD THREE (Mist)(Yellow Border)
     $("#cloud-3").click(function(){
       $(".cloud").fadeOut(1000, function(){
-        $("#sun").fadeIn().animate({top: '100px'}, 1000).fadeOut();
+        $("#sun").fadeIn().animate({top: '100px'}, 1000).fadeOut(); // Display Sunrise
       });
       $('body').css('background-color', "rgb(149,202,255)");
-      $("#text").text("It's sunny! Go outside!").fadeIn(1000);
+      $("#text").text("It's sunny! Go outside!").fadeIn(1000); // End Text
     });
 
-    // GREEN Flat Bottom Cloud
-    //$("#cloud-4").hide();
+    // CLOUD FOUR (Straight Bottom)(Green Border)
     $("#cloud-4").click(function(){
       $("#cloud-4").animate({right: '250px', top: '100px'}).fadeOut(1000);
     });
 
-    // BLUE Patchy Circle
+    // CLOUD FIVE (Patchy Circle)(Blue Border)
     $("#cloud-5").click(function(){
-      //  $(allClouds[0].theSelector).src = allClouds[random].imagePath;
       $("#cloud-5").animate({left: '-=50', top: '-=50'}, 1000);
         $("#cloud-5").fadeOut(1000);
-    });
-
-    $("#clickAround").hover(function(){
-      $("#clickAround").fadeOut(1500);
     });
 });
