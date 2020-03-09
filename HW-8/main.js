989cbb20cf13430010e1bd69398a8f686bfa98e3
Katherine <holmgrenkatherine@gmail.com>
2020-03-09 15:41:59 -0600

    Finished Jquery main

--- HW-8/main.js
+++ HW-8/main.js
@@ -1,75 +1,46 @@
+$(function(){
 
+});
 
 $(document).ready(function(){
+
+  $("#cloud-3").hide();
+  $("#sun").hide();
+    // RED Dark Right Hand Corner
     $("#cloud-1").click(function(){
-      //  $(allClouds[0].theSelector).src = allClouds[random].imagePath;
-        $("#cloud-1").fadeOut().fadeIn();
+        $("#cloud-1").animate({left: '250px'}, 1000);
+        $("#cloud-1").fadeOut(1000);
     });
+
+    // ORANGE Dark Bottom
     $("#cloud-2").click(function(){
-      //  $(allClouds[0].theSelector).src = allClouds[random].imagePath;
-        $("#cloud-2").fadeOut();
+        $("#cloud-2").animate({right: '250px'}).fadeOut(1000, function(){
+          $("#cloud-3").fadeIn("slow");
+        });
     });
+
     $("#cloud-3").click(function(){
-      //  $(allClouds[0].theSelector).src = allClouds[random].imagePath;
-        $(".cloud").fadeOut();
-        $("#ohNo").text("You better stop coding! The sun is out!");
+      $(".cloud").fadeOut(1000, function(){
+        $("#sun").fadeIn().animate({top: '100px'}, 1000).fadeOut();
+      });
+      $('body').css('background-color', "rgb(149,202,255)");
+      $("#text").text("It's sunny! Go outside!").fadeIn(1000);
     });
+
+    // GREEN Flat Bottom Cloud
+    //$("#cloud-4").hide();
     $("#cloud-4").click(function(){
-      //  $(allClouds[0].theSelector).src = allClouds[random].imagePath;
-        $("#cloud-4").fadeOut().fadeIn();
+      $("#cloud-4").animate({right: '250px', top: '100px'}).fadeOut(1000);
     });
+
+    // BLUE Patchy Circle
     $("#cloud-5").click(function(){
       //  $(allClouds[0].theSelector).src = allClouds[random].imagePath;
-        $("#cloud-5").fadeOut().fadeIn().fadeOut();
+      $("#cloud-5").animate({left: '-=50', top: '-=50'}, 1000);
+        $("#cloud-5").fadeOut(1000);
+    });
+
+    $("#clickAround").hover(function(){
+      $("#clickAround").fadeOut(1500);
     });
 });
-
-
-
-
-// OLD CODE FOR SINGLE ROTATING CLOUD (does not work)
-/* var cloudSelector = "#cloud";
-var allClouds = new Array();
-var cloudImg1 = "./imgs/cloud-1.png";
-var cloudImg2 = "./imgs/cloud-2.png";
-var cloudImg3 = "./imgs/cloud-3.png";
-var cloudImg4 = "./imgs/cloud-4.png";
-var cloudImg5 = "./imgs/cloud-5.png";
-
-class CloudInfo{
-    constructor(selector, imagePath){
-        this.selector = selector;
-        this.imagePath = imagePath;
-    }
-    get theSelector(){
-        return this.selector;
-    }
-    get theImagePath(){
-        return this.imagePath;
-    }
-    toString(){
-        return this.selector + ":" + this.imagePath;
-    }
-}
-
-function initializeArray(){
-    var cloud1 = new CloudInfo("#cloud", cloudImg1);
-    var cloud2 = new CloudInfo("#cloud", cloudImg2);
-    var cloud3 = new CloudInfo("#cloud", cloudImg3);
-    var cloud4 = new CloudInfo("#cloud", cloudImg4);
-    var cloud5 = new CloudInfo("#cloud", cloudImg5);
-    allClouds.push(cloud1);
-    allClouds.push(cloud2);
-    allClouds.push(cloud3);
-    allClouds.push(cloud4);
-    allClouds.push(cloud5);
-}
-
-$(document).ready(function(){
-    var random = Math.floor(Math.random() * allClouds.length);
-    $("#cloud").click(function(){
-        $(allClouds[0].theSelector).src = allClouds[random].imagePath;
-        $(allClouds[random].theSelector).fadeOut().fadeIn();
-    });
-});
-*/
