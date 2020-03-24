dd390d901aa99fb11eac48adee0c19099ddcd66d
Katherine <holmgrenkatherine@gmail.com>
2020-03-23 19:19:05 -0800

    Stuck

--- HW-9/scripts.js
+++ HW-9/scripts.js
@@ -2,10 +2,11 @@
 $(document).ready(function () {
 
 $("button").click(function() {
-  $.getJSON("prize.json", function(result) {
+  $.getJSON("prize.json", function(result) { // Gets info from json file
     $.each(result, function(i, field) {
-      $("#prizeInfo").append(field + " ");
+      //var prize = _____?
+       $("#prizeInfo").append(field + " "); // What should I add in this section to make it work?
+      });
     });
-});
-});
+  });
 });
