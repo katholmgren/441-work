36ae7fd7e58f7e48641494c5e475f1f70af53a5c
Katherine <holmgrenkatherine@gmail.com>
2020-03-30 12:23:30 -0800

    Colliding in wrong spot

--- HW-10/scripts.js
+++ HW-10/scripts.js
@@ -1,16 +1,22 @@
+//https://www.webfx.com/blog/web-design/bouncing-a-ball-around-with-html5-and-javascript/
+
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
 var userX = 50;
 var userY = 50;
+var width = 50;
+var height = 50;
 var x = 300;
 var y = 200;
 var dx=5; // For green square
 var dy=5;
 var randomNumber = Math.floor(Math.random() * 10)
 drawSquare();
-hasCollided();
-setInterval(update, 1000/60);
+setInterval(draw, 1000/60);
+
+var collide = new Audio();
+collide.src = "sounds/happy-quick.wav";
 
 $(document).ready(function(){
     $(this).keypress(function(event){
@@ -36,24 +42,35 @@ function getKey(event){ // Controls for blue square
     drawSquare();
 }
 
-function update(){ // Using this so I could set a framerate (see vars)
+function draw(){ // Using this so I could set a framerate (see vars)
      drawSquare();
+     ctx.clearRect(0,0,600,400);
+     ctx.fillStyle = "#0000FF";
+     ctx.fillRect(userX, userY, width, height); // User-controlled square
+     ctx.fillStyle = "green";
+     ctx.fillRect(x, y, width, height); // Free green square
      x+= dx; // Green square movement
      y+= dy;
      if(x<0 || x>600) dx=-dx;
      if(y<0 || y>400) dy=-dy;
      if(userX<0 || userX>600) userX= 300;
      if(userY<0 || userY>400) userY= 200;
-     x+=dx;
-     y+=dy;
+     // blue bottom touches green top || blue top touches green bottom ||blue right touches green left || blue left touches green right
+     if (((userY + height) === (y)) && (userY === (y + height)) && ((userX + width) === x) && (userX === (x + width))){
+       document.body.style.backgroundColor = "red";
+       collide.play();
+        } else {
+        return false;
+        document.body.style.backgroundColor = "white";
+      }
 }
 
 function drawSquare(){ // My lovely blue squares
     ctx.clearRect(0,0,600,400);
     ctx.fillStyle = "#0000FF";
-    ctx.fillRect(userX, userY, 20, 20); // User-controlled square
+    ctx.fillRect(userX, userY, width, height); // User-controlled square
     ctx.fillStyle = "green";
-    ctx.fillRect(x, y, 20, 20); // Free green square
+    ctx.fillRect(x, y, width, height); // Free green square
 }
 
 function moveUp(){
@@ -72,9 +89,10 @@ function moveLeft(){
     userX-=10;
 }
 
-function hasCollided() {
-    if (((userY + 20) < (y)) || (userY > (y + 20)) || ((userX + 20) < x) || (userX > (x + 20))){
-      x-=dx;
-      y-=dy;
-    }
-}
+// function hasCollided() {
+//       x-=5;
+//       dy = -5;
+//       backgroundcolor = "rgb(40,150,200)";
+//       // height+= 1;
+//       // width+= 1;
+// }
