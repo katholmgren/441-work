85ce9e3058de0924cba6cac336e29685851554cc
Katherine <holmgrenkatherine@gmail.com>
2020-03-30 15:21:16 -0800

    finished

--- HW-10/scripts.js
+++ HW-10/scripts.js
@@ -1,23 +1,24 @@
-//https://www.webfx.com/blog/web-design/bouncing-a-ball-around-with-html5-and-javascript/
 
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-var userX = 50;
-var userY = 50;
-var width = 50;
-var height = 50;
-var x = 300;
-var y = 200;
-var dx=5; // For green square
+const img = new Image()
+img.src = "./imgs/landscape.jpg"
+img.onload = () => {
+  ctx.drawImage(img, 0, 0)
+}
+
+var user = {x: 50, y: 50, w: 50, h: 50}
+var green = {x: 300, y: 200, w: 50, h: 50}
+var dx=5; // Delta for green square
 var dy=5;
-var randomNumber = Math.floor(Math.random() * 10)
-drawSquare();
 setInterval(draw, 1000/60);
 
+// Sounds
 var collide = new Audio();
 collide.src = "sounds/happy-quick.wav";
 
+// Not sure what this does exactly
 $(document).ready(function(){
     $(this).keypress(function(event){
         getKey(event);
@@ -42,57 +43,52 @@ function getKey(event){ // Controls for blue square
     drawSquare();
 }
 
-function draw(){ // Using this so I could set a framerate (see vars)
+function draw(){
      drawSquare();
      ctx.clearRect(0,0,600,400);
      ctx.fillStyle = "#0000FF";
-     ctx.fillRect(userX, userY, width, height); // User-controlled square
+     ctx.fillRect(user.x, user.y, user.w, user.h); // User-controlled blue square
      ctx.fillStyle = "green";
-     ctx.fillRect(x, y, width, height); // Free green square
-     x+= dx; // Green square movement
-     y+= dy;
-     if(x<0 || x>600) dx=-dx;
-     if(y<0 || y>400) dy=-dy;
-     if(userX<0 || userX>600) userX= 300;
-     if(userY<0 || userY>400) userY= 200;
-     // blue bottom touches green top || blue top touches green bottom ||blue right touches green left || blue left touches green right
-     if (((userY + height) === (y)) && (userY === (y + height)) && ((userX + width) === x) && (userX === (x + width))){
-       document.body.style.backgroundColor = "red";
-       collide.play();
-        } else {
-        return false;
-        document.body.style.backgroundColor = "white";
-      }
+     ctx.fillRect(green.x, green.y, green.w, green.h); // Free green square
+     green.x+= dx; // Green square movement
+     green.y+= dy;
+
+     if(green.x<0 || green.x>600) dx=-dx; // Stay in the canvas
+     if(green.y<0 || green.y>400) dy=-dy;
+     if(user.x<0 || user.x>600) user.x= 300;
+     if(user.y<0 || user.y>400) user.y= 200;
+
+     if (user.x < green.x + green.w &&
+        user.x + user.w > green.x &&
+        user.y < green.y + green.h &&
+        user.y + user.h > green.y) {
+         // collision detected!
+         collide.play();
+         green.w++;
+         green.h++;
+     }
 }
 
 function drawSquare(){ // My lovely blue squares
     ctx.clearRect(0,0,600,400);
     ctx.fillStyle = "#0000FF";
-    ctx.fillRect(userX, userY, width, height); // User-controlled square
+    ctx.fillRect(user.x, user.y, user.w, user.h); // User-controlled square
     ctx.fillStyle = "green";
-    ctx.fillRect(x, y, width, height); // Free green square
+    ctx.fillRect(green.x, green.y, green.w, green.h); // Free green square
 }
 
 function moveUp(){
-    userY-=10;
+    user.y-=10;
 }
 
 function moveDown(){
-    userY+=10;
+    user.y+=10;
 }
 
 function moveRight(){
-    userX+=10;
+    user.x+=10;
 }
 
 function moveLeft(){
-    userX-=10;
+    user.x-=10;
 }
-
-// function hasCollided() {
-//       x-=5;
-//       dy = -5;
-//       backgroundcolor = "rgb(40,150,200)";
-//       // height+= 1;
-//       // width+= 1;
-// }
