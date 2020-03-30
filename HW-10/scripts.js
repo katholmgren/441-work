b55bef62222f0349e37c2d0ac0f1b7d695c47291
Katherine <holmgrenkatherine@gmail.com>
2020-03-29 22:18:38 -0800

    constrained in canvas

--- HW-10/scripts.js
+++ HW-10/scripts.js
@@ -1,11 +1,15 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+
 var userX = 50;
 var userY = 50;
 var x = 300;
 var y = 200;
+var dx=5; // For green square
+var dy=5;
 var randomNumber = Math.floor(Math.random() * 10)
 drawSquare();
+hasCollided();
 setInterval(update, 1000/60);
 
 $(document).ready(function(){
@@ -34,9 +38,14 @@ function getKey(event){ // Controls for blue square
 
 function update(){ // Using this so I could set a framerate (see vars)
      drawSquare();
-     x+= randomNumber; // Green square movement
-     y+= randomNumber;
-
+     x+= dx; // Green square movement
+     y+= dy;
+     if(x<0 || x>600) dx=-dx;
+     if(y<0 || y>400) dy=-dy;
+     if(userX<0 || userX>600) userX= 300;
+     if(userY<0 || userY>400) userY= 200;
+     x+=dx;
+     y+=dy;
 }
 
 function drawSquare(){ // My lovely blue squares
@@ -63,14 +72,9 @@ function moveLeft(){
     userX-=10;
 }
 
-
-
-
-// function hasCollided(object1, object2) {
-//     return !(
-//         ((object1.y + object1.height) < (object2.y)) ||
-//         (object1.y > (object2.y + object2.height)) ||
-//         ((object1.x + object1.width) < object2.x) ||
-//         (object1.x > (object2.x + object2.width))
-//     );
-// }
+function hasCollided() {
+    if (((userY + 20) < (y)) || (userY > (y + 20)) || ((userX + 20) < x) || (userX > (x + 20))){
+      x-=dx;
+      y-=dy;
+    }
+}
