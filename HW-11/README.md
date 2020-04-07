4264f1d12729de84d435d8a995d9f288043f76e9
Katherine <holmgrenkatherine@gmail.com>
2020-04-06 21:03:28 -0800

    Finished for now

---
+++ HW-11/README.md
@@ -0,0 +1,27 @@
+Katherine Holmgren
+
+April 6, 2020
+
+# HW-11 Response
+
+[HW-11 Live Link](https://katholmgren.github.io/441-work/HW-11)
+
+For this week's homework, we worked with the HTML5 canvas and object collisions. Understanding collisions was a challenge for me, but I eventually got it working. For the creative part, I had grown attached to the squares so I decided to keep them and just add a background image. It's simple, but I like it. It ended up kind of abstract.
+
+### Process
+
+1. Set up files
+2. Draw squares
+3. Make blue square user-controlled
+4. Make green square move freely
+5. Add border restraints
+6. Add sound
+6. Struggle with collisions for a hot minute
+7. Sweet victory over collisions
+8. Add background image
+
+### Difficulties
+
+The collision part was hard to understand! I couldn't get it working with the code from the classwork, but I found a helpful example online at [developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection](https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection) and got it working after a while.
+
+Another resource I used for the canvas border restraints was  [www.webfx.com/blog/web-design/bouncing-a-ball-around-with-html5-and-javascript/](https://www.webfx.com/blog/web-design/bouncing-a-ball-around-with-html5-and-javascript/).
--- HW-11/scripts/canvas.js
+++ HW-11/scripts/canvas.js
@@ -3,12 +3,16 @@ var ctx;
 var x = 50;
 var y = 50;
 var dx = 5;
-var dy = 5;
+var dy = Math.floor(Math.random() * 10);
 var square1, square2;
-var direction;
-var questions;
 var squareArray = [];
 var lives = 3;
+
+const img = new Image()
+img.src = "./imgs/squares-abstract.jpg"
+img.onload = () => {
+  ctx.drawImage(img, 0, 0)
+}
 
 $(document).ready(function(){
     setup();
@@ -25,8 +29,8 @@ function setup(){
     //drawSquare();
 
     // create two objects
-    square1 = new Square(100,100,50,50,"#0000FF");
-    square2 = new Square(400,400,50,50,"#00FF00");
+    square1 = new Square(100,100,70,70, "#00008b");
+    square2 = new Square(400,400,60,60, "#ffff00");
     $.getJSON("information.json", function(data) {
         for(var i = 0; i < data.squares.length; i++){
             squareArray.push(new Square(data.squares[i].x,data.squares[i].y, data.squares[i].h, data.squares[i].w, data.squares[i].color));
@@ -46,11 +50,12 @@ function draw(){
          ctx.fillRect(squareArray[i].x, squareArray[i].y, squareArray[i].w, squareArray[i].h);
          squareArray[i].x+= dx;
          squareArray[i].y+= dy;
+         if(squareArray[i].x<0 || squareArray[i].x>600) squareArray[i].x=-dx; // Stay in the canvas
+         if(squareArray[i].y<0 || squareArray[i].y>400) squareArray[i].y=-dy;
      }
      square2.x+= dx; // Green square movement
      square2.y+= dy;
-     // data.squares.x+= dx;
-     // data.squares.y+= dy;
+
 
      if(square2.x<0 || square2.x>600) dx=-dx; // Stay in the canvas
      if(square2.y<0 || square2.y>400) dy=-dy;
@@ -62,9 +67,19 @@ function draw(){
         square1.y < square2.y + square2.h &&
         square1.y + square1.h > square2.y) {
          // collision detected!
-         // collide.play();
+         //collide.play();
+         console.log("Success")
          square2.w++;
          square2.h++;
+//          if (user.x < green.x + green.w &&
+//    user.x + user.w > green.x &&
+//    user.y < green.y + green.h &&
+//    user.y + user.h > green.y) {
+//     // collision detected!
+//     collide.play();
+//     green.w++;
+//     green.h++;
+// }
      }
 }
 
@@ -97,3 +112,13 @@ function getKey(event){
         square1.x+=10;
     }
 }
+
+// This does not work:
+// function hasCollided(object1, object2) {
+//     return !(
+//         ((object1.y + object1.h) < (object2.y)) ||
+//         (object1.y > (object2.y + object2.h)) ||
+//         ((object1.x + object1.w) < object2.x) ||
+//         (object1.x > (object2.x + object2.w))
+//     );
+// }
