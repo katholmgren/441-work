a7445a7713acace9560be40a829c67d649fc4f8b
Katherine <holmgrenkatherine@gmail.com>
2020-04-20 16:41:19 -0800

    Finished

---
+++ HW-13/README.md
@@ -0,0 +1,26 @@
+Katherine Holmgren
+
+April 20, 2020
+
+# HW-13 Response
+
+[HW-13 Live Link](https://katholmgren.github.io/441-work/HW-13)
+
+This week we used three.js to experiment with working in 3d. This was new to me, so there was a lot of guess-and-check and googling this week. I ran into many issues, but now I have a basic understanding of what three.js can do. Because I'm not in the BFA, I've never used Maya or have made a model before.
+
+### Process
+
+1. Add a cube
+2. Add a cone
+3. Add rotation
+4. Research mouse controls
+5. Work with First Person Controls...
+5. Work with Trackball Controls
+6. Disable animated rotation
+7. Tried to separate the cube and cone
+8. Tried to add a background image
+9. Tried to add a border to the shapes
+
+### Difficulties
+
+There were many things in three.js I didn't understand. Even simple things like adding a border to the shapes, repositioning the shapes, or adding a background image were more complicated than I expected them to be. I'm glad I got the shapes to rotate with the mouse, but I wish I had understood Three better so I could have made something more than a cube and a cone overlapping. I see the potential, though.
--- HW-13/script.js
+++ HW-13/script.js
@@ -1,23 +1,26 @@
 
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera = new THREE.PerspectiveCamera (75, window.innerWidth / window.innerHeight);
+// document.getElementById("WebGL-output").appendChild(renderer.domElement);
+
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setClearColor('white');
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+
+controls = new THREE.TrackballControls( camera, renderer.domElement );
+controls.target.set( 0, 0, 0 )
+
 // ^ Setup --------------
-
-
 
 var geometry = new THREE.BoxGeometry();
 var material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00
+    color: "rgb(160, 99, 156)"
 });
-
-var geometry2 = new THREE.ConeGeometry( 1, 5, 20 );
-var material2 = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+var geometry2 = new THREE.ConeGeometry( 1, 6, 20 );
+var material2 = new THREE.MeshBasicMaterial( {color: "rgb(187, 255, 255)"} );
 var cone = new THREE.Mesh( geometry2, material2 );
 scene.add( cone );
 
@@ -29,10 +32,11 @@ camera.position.z = 5;
 
 function animate() {
     requestAnimationFrame( animate );
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    cone.rotation.x += 0.01;
-    cone.rotation.y += 0.01;
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
+    // cone.rotation.x += 0.01;
+    // cone.rotation.y += 0.01;
+    controls.update();
     renderer.render( scene, camera );
 }
 animate();
