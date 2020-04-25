121811b7f1178d7703484d32c7f502c1c57daa88
Katherine <holmgrenkatherine@gmail.com>
2020-04-25 15:55:32 -0800

    Get mouse coordinates in console.log

--- final/README.md
+++ final/README.md
@@ -1,4 +1,6 @@
 <!-- https://stackoverflow.com/questions/14824747/overlay-html5-canvas-over-image -->
+<!-- https://stackoverflow.com/questions/55677/how-do-i-get-the-coordinates-of-a-mouse-click-on-a-canvas-element/18053642#18053642 -->
+<!-- Partriques Answer -->^^
 
 1. Took pictures
 2. Set up canvas with background images
--- final/scripts.js
+++ final/scripts.js
@@ -1,10 +1,25 @@
 
 $(document).ready(function () {
-    $("#office").hide(); //Originally hidden
+
+    $("#office").hide(); // Originally hidden
 
     $(".coveringCanvas").click(function () {
         $("kitchen").fadeOut();
         $("#office").fadeIn();
         $("#description").text("Scissors, seeds, and cards. Look very hard.").fadeIn(1000);
     });
-});
+
+    function getCursorPosition(canvas, event) { // Get coordinates
+        const rect = canvas.getBoundingClientRect()
+        const x = event.clientX - rect.left
+        const y = event.clientY - rect.top
+        console.log("x: " + x + " y: " + y)
+    }
+
+    const canvas = document.querySelector('canvas')
+        canvas.addEventListener('mousedown', function(e) { // If mouse is down...
+            getCursorPosition(canvas, e) // Then get x, y
+    })
+
+
+}); // End document.ready
