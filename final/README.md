dc718724f13675c89289df0743ebbf252df06d4e
Katherine <holmgrenkatherine@gmail.com>
2020-04-25 16:17:00 -0800

    If coordinate range is clicked, proceed

--- final/README.md
+++ final/README.md
@@ -1,7 +1,9 @@
 <!-- https://stackoverflow.com/questions/14824747/overlay-html5-canvas-over-image -->
 <!-- https://stackoverflow.com/questions/55677/how-do-i-get-the-coordinates-of-a-mouse-click-on-a-canvas-element/18053642#18053642 -->
 <!-- Partriques Answer -->^^
+<!-- Coordinate grid system: https://softwareengineering.stackexchange.com/questions/187880/algorithm-to-detect-a-click-within-square-range -->
 
 1. Took pictures
 2. Set up canvas with background images
 3. Transitioned between background and texts on click (with JQuery)
+4. Got mouse coordinate onclick
--- final/scripts.js
+++ final/scripts.js
@@ -3,23 +3,28 @@ $(document).ready(function () {
 
     $("#office").hide(); // Originally hidden
 
-    $(".coveringCanvas").click(function () {
-        $("kitchen").fadeOut();
-        $("#office").fadeIn();
-        $("#description").text("Scissors, seeds, and cards. Look very hard.").fadeIn(1000);
-    });
+    // $(".coveringCanvas").click(function () {
+    //     $("kitchen").fadeOut();
+    //     $("#office").fadeIn();
+    //     $("#description").text("Scissors, seeds, and cards. Look very hard.").fadeIn(1000);
+    // });
 
     function getCursorPosition(canvas, event) { // Get coordinates
         const rect = canvas.getBoundingClientRect()
-        const x = event.clientX - rect.left
-        const y = event.clientY - rect.top
-        console.log("x: " + x + " y: " + y)
+        var mx = event.clientX - rect.left // Mouse X
+        var my = event.clientY - rect.top // Mouse Y
+        console.log("x: " + mx + " y: " + my)
+        if (mx>200 && mx<400 && my>100 && my<300){// A mouse click at mx, my is in the square if (mx>x1 and mx<x2 and my>y1 and my<y2)
+            console.log("success");
+            $("kitchen").fadeOut();
+            $("#office").fadeIn();
+            $("#description").text("Scissors, seeds, and cards. Look very hard.").fadeIn(1000);
+        }
     }
 
     const canvas = document.querySelector('canvas')
         canvas.addEventListener('mousedown', function(e) { // If mouse is down...
             getCursorPosition(canvas, e) // Then get x, y
     })
-
 
 }); // End document.ready
